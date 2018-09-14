933d2d75bd56c72f4724ba4cf24e26a0554605c5
Stepan Kurennykh <stoope@outlook.com>
2018-09-14 21:59:06 +0300

    webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,15 +2,13 @@ const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 const safeParser = require("postcss-safe-parser");
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: path.join(__dirname, "src/index.html"),
-  filename: path.join(__dirname, "dist/index.html")
-});
-
 module.exports = {
+  mode: "production",
+  devtool: "source-map",
   entry: path.join(__dirname, "src/index.js"),
   output: {
     path: path.join(__dirname, "dist"),
@@ -25,8 +23,8 @@ module.exports = {
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
-        cache: true,
         parallel: true,
+        cache: true,
         sourceMap: true
       }),
       new OptimizeCSSAssetsPlugin({
@@ -40,40 +38,66 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["thread-loader", { loader: "babel-loader" }]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
+        oneOf: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["thread-loader", "babel-loader"]
+          },
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[ext]",
+                  outputPath: "fonts/"
+                }
+              }
+            ]
+          },
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "static/media/[name].[hash:8].[ext]"
+            }
+          },
           {
             loader: "file-loader",
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/"
+              name: "static/media/[name].[hash:8].[ext]"
             }
           }
         ]
-      },
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "url-loader",
-        options: {
-          limit: 100,
-          name: "static/media/[name].[hash:8].[ext]"
-        }
       }
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      minify: true
+    }),
     new MiniCssExtractPlugin({
       filename: "index.css"
     }),
-    htmlPlugin
-  ]
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$|\.svg$/
+    })
+  ],
+  node: {
+    dgram: "empty",
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
+  }
 };
